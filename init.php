9816b5abf6991e4069285cd126cd6cbf0872bc55
Eric <ekumlin@outlook.com>
2014-12-07 11:57:09 -0700

    Added template to init; moved DB util file

--- init.php
+++ init.php
@@ -5,7 +5,8 @@ define('_ROOT', rtrim(realpath(dirname(__FILE__)), '/') . '/');
 global $CONFIG, $DB;
 
 require(_ROOT . 'config.php');
-require(_ROOT . 'util/db.php');
+require(_ROOT . 'util/database.php');
+require(_ROOT . 'util/template.php');
 
 $DB = new Connection($CONFIG['db-host'], $CONFIG['db-user'], $CONFIG['db-pass'], $CONFIG['db-name']);
 
