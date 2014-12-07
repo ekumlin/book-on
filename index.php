cba750ed6c6f5cd58271f9dc3561191ef58af3a1
Eric <eric.kumlin@gmail.com>
2014-12-07 15:58:21 -0700

    Added book index (temporary, should use API)

--- index.php
+++ index.php
@@ -2,4 +2,30 @@
 
 require('init.php');
 
+$bookIndex = '';
+
+$query = <<<EOD
+SELECT
+	b.*, a.*
+FROM
+	Book AS b
+	LEFT JOIN BookAuthor AS ba ON ba.ISBN = b.ISBN
+	LEFT JOIN Author AS a ON ba.AuthorId = a.AuthorId
+	LEFT JOIN Publisher AS p ON p.PublisherId = b.Publisher
+EOD;
+$books = $DB->query($query);
+
+foreach ($books as $row) {
+	$bookIndex .= Template::toString("bookCard", array(
+		'book' => new Book($row, true),
+	));
+}
+
+print Template::toString("page", array(
+	'title' => 'Book-On',
+	'styles' => array('base'),
+	'scripts' => array(''),
+	'body' => $bookIndex,
+));
+
 ?>
