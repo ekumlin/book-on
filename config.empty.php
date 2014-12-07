a3e8fcb5d44690343aeee64f85038c6ec4b2f585
Eric <ekumlin@outlook.com>
2014-12-07 11:11:36 -0700

    Adding empty config file

---
+++ config.empty.php
@@ -0,0 +1,11 @@
+<?php
+
+// Edit me and save me as config.php!
+// Do not commit any config files!
+
+$CONFIG['db-host'] = '';
+$CONFIG['db-user'] = '';
+$CONFIG['db-pass'] = '';
+$CONFIG['db-name'] = '';
+
+?>
