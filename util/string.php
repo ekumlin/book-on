4306851c9cbb4dc5b5562502f2c2604a560c4215
Eric <ekumlin@outlook.com>
2014-12-09 21:46:41 -0700

    Formatted ISBN in bookView

--- util/string.php
+++ util/string.php
@@ -23,6 +23,21 @@ class String {
 		$ends = array('th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th');
 		return strval($number) . $ends[$value % 10];
 	}
+
+	/**
+	 * Formats either an ISBN-13 or ISBN-10 for human reading.
+	 *
+	 * @param string $number The ISBN number. Can be an integer or string.
+	 */
+	public static function formatIsbn($isbn) {
+		$isbn = strval($isbn);
+		
+		if (strlen($isbn) == 13) {
+			return substr($isbn, 0, 3) . '-' . substr($isbn, 3);
+		}
+
+		return $isbn;
+	}
 }
 
 ?>
