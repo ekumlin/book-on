42b1decb683f44a1cf6daa7163d5ee4f16b0e536
Eric <eric.kumlin@gmail.com>
2014-12-18 09:27:55 -0700

    Fixed weird query

--- controllers/collectionController.php
+++ controllers/collectionController.php
@@ -141,11 +141,11 @@ class CollectionController {
 				p.Name AS PublisherName,
 				SUM(CASE WHEN bc.IsForSale = 0 AND bc.HeldBy IS NULL THEN 1 ELSE 0 END) AS CopiesForRent,
 				SUM(CASE WHEN bc.IsForSale = 1 AND bc.HeldBy IS NULL THEN 1 ELSE 0 END) AS CopiesForSale,
-				SUM(DISTINCT CASE WHEN br.Rating = 5 THEN 1 ELSE 0 END) AS Rated5Count,
-				SUM(DISTINCT CASE WHEN br.Rating = 4 THEN 1 ELSE 0 END) AS Rated4Count,
-				SUM(DISTINCT CASE WHEN br.Rating = 3 THEN 1 ELSE 0 END) AS Rated3Count,
-				SUM(DISTINCT CASE WHEN br.Rating = 2 THEN 1 ELSE 0 END) AS Rated2Count,
-				SUM(DISTINCT CASE WHEN br.Rating = 1 THEN 1 ELSE 0 END) AS Rated1Count
+				br.Rated5Count,
+				br.Rated4Count,
+				br.Rated3Count,
+				br.Rated2Count,
+				br.Rated1Count
 			FROM
 				Collection AS c
 				LEFT JOIN BookCollected AS bcol ON bcol.CollectionId = c.CollectionId
@@ -154,7 +154,17 @@ class CollectionController {
 				LEFT JOIN BookCopy AS bc ON bc.ISBN = b.ISBN
 				LEFT JOIN BookAuthor AS ba ON ba.ISBN = b.ISBN
 				LEFT JOIN Author AS a ON a.AuthorId = ba.AuthorId
-				LEFT JOIN BookRated AS br ON br.ISBN = b.ISBN
+				LEFT JOIN (
+					SELECT
+						ISBN,
+						SUM(CASE WHEN Rating = 5 THEN 1 ELSE 0 END) AS Rated5Count,
+						SUM(CASE WHEN Rating = 4 THEN 1 ELSE 0 END) AS Rated4Count,
+						SUM(CASE WHEN Rating = 3 THEN 1 ELSE 0 END) AS Rated3Count,
+						SUM(CASE WHEN Rating = 2 THEN 1 ELSE 0 END) AS Rated2Count,
+						SUM(CASE WHEN Rating = 1 THEN 1 ELSE 0 END) AS Rated1Count
+					FROM BookRated
+					GROUP BY ISBN
+				) AS br ON br.ISBN = b.ISBN
 			WHERE
 				{$conditionString}
 				c.CardNumber = :cardNumber
